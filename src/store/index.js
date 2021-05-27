674b55081dc9f2662d6ed56a2ae8183514440250
TarasBalem <balemtaras@gmail.com>
2021-05-27 17:03:17 +0300

    ready devTools

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,12 @@
 import {createStore} from "redux";
+import {devToolsEnhancer} from "redux-devtools-extension";
 import reducers from "../reducers";
 
-const store = createStore(reducers);
+const store = createStore(
+  reducers,
+  devToolsEnhancer({trace: true})
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 window.store = store;
 
 export default store;

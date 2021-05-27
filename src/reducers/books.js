0cbcf59817e06a9fc17e2cb4b8e5307312e11522
Taras Balem <balemtaras@gmail.com>
2021-05-27 14:10:06 +0300

    ready create store

---
+++ src/reducers/books.js
@@ -0,0 +1,6 @@
+import {defaultBooks} from "../books-db";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state = defaultBooks, action) => {
+  return state;
+};
---
+++ src/reducers/categoriesBooks.js
@@ -0,0 +1,6 @@
+import {categories} from "../books-db";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state = categories, action) => {
+  return state;
+};
